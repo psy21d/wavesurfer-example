e45f12fa34a907f6870a925eb34302795033055f
psy21d <psy21d@yandex.ru>
2021-07-10 16:38:59 +0300

    feat: volume knob

--- src/components/MultiTrackPlayer.vue
+++ src/components/MultiTrackPlayer.vue
@@ -1,22 +1,34 @@
 <template>
-  <wavesurfer 
+<div class="playerbox">
+  <div 
+    class="track" 
     v-for="player in Object.keys(playersOptions)" 
-    :src="playersOptions[player].file" 
-    :options="playersOptions[player].options" 
-    :key="playersOptions[player].name"
-    :ref="playersOptions[player].name"
+    :key="player"
   >
-  </wavesurfer>
+    <div><knob v-model="volume[player]" :min="0" :max="200"></knob></div>
+    <div>
+      <wavesurfer
+        :src="playersOptions[player].file" 
+        :options="playersOptions[player].options"
+        :ref="playersOptions[player].name"
+      >
+      </wavesurfer>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
   /* eslint-disable */
 import Cursor from "wavesurfer.js/dist/plugin/wavesurfer.cursor";
-import { watch, ref } from 'vue';
+import { watch, ref, reactive } from 'vue';
+import Knob from 'primevue/knob';
 
 export default {
   name: 'MTP',
-
+  components: {
+    Knob
+  },
   props: {
     playersOptions: {
       type: Object,
@@ -35,12 +47,13 @@ export default {
       }
     }
   },
-
   setup(props) {
     const playerNames = Object.keys(props.playersOptions)
     let players = {}
     let playersOptions = ref(props.playersOptions)
     let ready = {}
+    let volume = null;
+    let v = {}
 
     playerNames.forEach(playerName => {
       players[playerName] = ref(null);
@@ -48,8 +61,18 @@ export default {
       playersOptions.value[playerName].options = {
         plugins: [Cursor.create()],
       },
-      playersOptions.value[playerName].ready = ref(false)      
+      playersOptions.value[playerName].ready = ref(false)
+      v[playerName] = 100
     });
+    
+    volume = reactive(v)
+
+    watch(volume, newVolume => {
+      console.log(newVolume)
+      Object.keys(newVolume).forEach(playerName => {
+        players[playerName].value.waveSurfer.setVolume(newVolume[playerName] / 100)
+      })
+    })
 
     let playersForWatch = []
     let readyForWatch = []
@@ -59,7 +82,6 @@ export default {
   
     let playersPlay = () => {
       console.log('play')
-
       Object.keys(players).forEach(player => {
         players[player].value.waveSurfer.setMute(false);
         console.log(players[player].value.waveSurfer.getVolume());
@@ -70,10 +92,8 @@ export default {
         }, 0);
       })
     }
-
     let playersStop = () => {
       console.log('stop')
-
       Object.keys(players).forEach(player => {
         players[player].value.waveSurfer.setMute(false);
         console.log(players[player].value.waveSurfer.getVolume());
@@ -84,7 +104,6 @@ export default {
         }, 0);
       })
     }    
-
     let checkToPlayersPlay = () => {
       const unactivatedFound = Object.keys(ready).find((active) => {
         return !ready[active].value
@@ -92,22 +111,18 @@ export default {
       if (unactivatedFound) return
       playersPlay()
     }
-
     watch(playersForWatch, (activatedPlayers) => {
       const unactivatedFound = activatedPlayers.find((player) => {
         return !player
       })
       if (unactivatedFound) return
-
       playerNames.forEach(playerName => {
         ready[playerName] = ref(playersOptions.value[playerName].ready) 
         players[playerName].value.name = playerName
       })
-
       Object.keys(ready).forEach(r => {
         readyForWatch.push(ready[r])
       })
-
       watch(readyForWatch, (activatedReady) => {
         const unactivatedFound = activatedReady.find((active) => {
           return (active === false)
@@ -116,9 +131,7 @@ export default {
         console.log('all players ready')
         playersPlay()
       });
-
       console.log('all players initialized')
-
       activatedPlayers.forEach(player => {
         player.waveSurfer.on('ready', () => {
           console.log('ready ' + player.name);
@@ -127,7 +140,6 @@ export default {
         })
       });
     })
-
     let play = () => {
       playersPlay()
     }
@@ -135,11 +147,21 @@ export default {
     let stop = () => {
       playersStop()
     }
-
-    return { ...players, playersOptions, ready, play, stop }
+    return { ...players, playersOptions, ready, play, stop, volume }
   }
 };
 </script>
 
 <style scoped>
+</style>
+
+<style scoped>
+.playerbox {
+  display: flex;
+  flex-direction: column;
+}
+.track {
+  display: flex;
+  flex-direction: row;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 import WaveSurferVue from "wavesurfer.js-vue";
- 
-createApp(App).use(WaveSurferVue).mount('#app')
+
+createApp(App).use(WaveSurferVue).use(PrimeVue).mount('#app')
